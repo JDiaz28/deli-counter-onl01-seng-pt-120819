6bc7cbb6c594a7f76ce5061c9c85ca2c5c02f958
Abraham Moreno <adiaz2821@gmail.com>
2019-12-23 14:33:58 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,12 @@
 # Write your code here.
+def line(array)
+  if array.length == 0 
+    puts "The line is currently empty."
+  else
+    message = "The line is currently: "
+    array.each_with_index do |names, index|
+    puts message += "#{index.to_i + 1}. #{names}"
+  end 
+  end
+end 
+
